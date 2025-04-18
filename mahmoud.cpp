01012f49ac19a2e35947adcb074acaefb7a77bb9
alihosny <hosnyali603@gmail.com>
2025-04-18 17:31:24 +0200

    files have been modified

---
+++ mahmoud.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"TESt"<<endl;
+
+    return 0;
+}
---
+++ qq.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"TESt ali hosny"<<endl;
+cout<<"show ";
+    return 0;
+}
