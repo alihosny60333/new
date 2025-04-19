2500cb14ad11d646b0c13b67f955e734677bde2f
alihosny <hosnyali603@gmail.com>
2025-04-19 18:25:46 +0200

    new branch

---
+++ mahmoud.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"deleted"<<endl;
+cout<<sqrt(5);
+    return 0;
+}
--- qq.cpp
+++ qq.cpp
@@ -3,6 +3,12 @@ using namespace std;
 int main()
 {
 cout<<"new branch"<<endl;
-
+int n;cin>>n;
+for(int i=2;i<n;i++)
+while(n%i==0)
+{
+cout<<i<<endl;
+n/=i;
+}
     return 0;
 }
